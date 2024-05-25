b79979a82b3e0902f5ff8a817b543bbee5d14c9f
Coimbra <adsjkbd@gmail.com>
2024-05-25 18:49:41 -0300

    testando a remoção de planeta

--- src/main/java/com/example/swplanetapi/services/PlanetService.java
+++ src/main/java/com/example/swplanetapi/services/PlanetService.java
@@ -1,8 +1,13 @@
 package com.example.swplanetapi.services;
 
 import com.example.swplanetapi.domain.Planet;
+import com.example.swplanetapi.domain.QueryBuilder;
 import com.example.swplanetapi.repositories.PlanetRepository;
+import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PlanetService {
@@ -15,5 +20,22 @@ public class PlanetService {
 
     public Planet create(Planet planet) {
         return planetRepository.save(planet);
+    }
+
+    public Optional<Planet> get(Long id) {
+        return planetRepository.findById(id);
+    }
+
+    public Optional<Planet> getByName(String name) {
+        return planetRepository.findByName(name);
+    }
+
+    public List<Planet> list(String terrain, String climate) {
+        Example<Planet> query = QueryBuilder.makeQuery(new Planet(climate, terrain));
+        return planetRepository.findAll(query);
+    }
+
+    public void remove(Long id) {
+        planetRepository.deleteById(id);
     }
 }
